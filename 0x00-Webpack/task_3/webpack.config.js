03e6194a0b8ddef3d46044b75a8c9979445ffd4d
Oussama-hamdi <ohamdi776@gmail.com>
2024-01-29 19:12:29 +0000

    Completed 3. Dev servers, modules, and tree shaking task

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production',
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+	  'file-loader',
+	  {
+	    loader: 'image-webpack-loader',
+	    options: {
+	      bypassOnDebug: true,
+	      disable: true,
+	    },
+	  },
+	],
+      },
+    ],
+  },
+  devServer: {
+    static: path.join(__dirname, './public'),
+    compress: true,
+    port: 8564,
+  },
+  devtool: 'inline-source-map',
+};
