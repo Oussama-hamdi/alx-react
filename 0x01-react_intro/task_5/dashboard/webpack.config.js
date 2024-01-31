73db5154049d5ba6780fcae16f5bc0d9c8fe8580
Oussama-hamdi <86133676+Oussama-hamdi@users.noreply.github.com>
2024-01-31 12:25:05 +0100

    Completed 9. Create a project using Webpack task

---
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  plugins: [new HtmlWebpackPlugin({ title: "Webpack" })],
+};
