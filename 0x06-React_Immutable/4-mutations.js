26017d3d9891975423da9bf3691774c0b45116fd
Oussama Hamdi <86133676+Oussama-hamdi@users.noreply.github.com>
2024-02-21 15:31:59 +0100

    Update 4-mutations.js

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-export const map1 = Map({
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,6 +9,6 @@ export const map1 = Map({
   6: 'Lucas',
 });
 
-export const map2 = map1.withMutations((val) => {
+export const map2 = map.withMutations((val) => {
   val.set(2, 'Benjamin').set(4, 'Oliver');
 });
